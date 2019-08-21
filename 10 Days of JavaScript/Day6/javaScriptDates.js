f4f48580b6b511591384c295e1e1958c14c992b2
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-21 05:47:49 -0400

    JavaScript Dates Solution added in JavaScript

---
+++ 10 Days of JavaScript/Day6/javaScriptDates.js
@@ -0,0 +1,16 @@
+// Objective
+// In this challenge, we learn about JavaScript Dates. Check out the attached tutorial for more details.
+// Task
+// Given a date string, , in the format MM/DD/YYYY, find and return the day name for that date. Each day name must be one of the following 
+// strings: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. For example, the day name for the date 12/07/2016 is Wednesday.
+
+// Solution
+
+function getDayName(dateString) {
+    let dayName;
+    let dn = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let d = new Date(dateString);
+    dayName = dn[d.getDay()];
+    return dayName;
+}
+
