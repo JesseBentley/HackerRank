b43ae4351dfd051e14bda9301ea91d5c1521e833
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-23 06:17:31 -0400

    2-3 uploaded

---
+++ 10 Days of JavaScript/Day7/regularExpressions3.js
@@ -0,0 +1,15 @@
+// Complete the function in the editor below by returning a RegExp object, , that matches every integer in some string .
+
+// Solution
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    let re = RegExp('\\d+','g');
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
